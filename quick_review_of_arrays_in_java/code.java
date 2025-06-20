c5d108a6f5f357d718a916e4817733bdfa3ae2f3
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-06-20 21:59:32 +0530

    array quick review

---
+++ quick_review_of_arrays_in_java/code.java
@@ -0,0 +1,14 @@
+public class code {
+          public static void main(String[] args){
+                    int[] arr = new int[5];
+                    arr[0]= 1;
+                    arr[1] = 2;
+                    arr[2] = 42;
+                    arr[3] = 32;
+                    arr[4] = 31;
+                    for(int i =0;i<arr.length;i++){
+                              System.out.println(i);
+                    }
+          }
+          
+}
