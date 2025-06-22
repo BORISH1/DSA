01f58f74997ef714ff7fd5dab8d975cfb0e9caf1
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-06-22 10:26:25 +0530

    haft tringle pattern

---
+++ quick_review_of_arrays_in_java/HalftrianglePattern.java
@@ -0,0 +1,12 @@
+package quick_review_of_arrays_in_java;
+
+public class HalftrianglePattern {
+    public static void main(String[] args){
+        for(int i = 0;i<4;i++){
+            System.out.println();
+            for(int j=0;j<=i;j++){
+                System.out.print('*');
+            }
+        }
+    }
+}
