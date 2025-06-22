e3e80f048a8b1e3b2cf882c7edb1c5a7e0dcdeb5
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-06-22 10:16:50 +0530

    square pattern

---
+++ quick_review_of_arrays_in_java/SquareStarPrint.java
@@ -0,0 +1,13 @@
+package quick_review_of_arrays_in_java;
+
+public class SquareStarPrint {
+    public static void main(String[] args){
+        for(int i = 0;i<=5;i++){
+            System.out.println();
+            for(int j = 0;j<=5;j++){
+                System.out.print('*');
+            }
+
+        }
+    }
+}
