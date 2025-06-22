02ad978eee775e0775542f97de62df9cb0c6f323
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-06-22 20:04:19 +0530

    swapping

---
+++ quick_review_of_arrays_in_java/productoftwonumber.java
@@ -0,0 +1,17 @@
+package quick_review_of_arrays_in_java;
+
+public class productoftwonumber {
+
+    public static int multiply(int a, int b) {
+
+
+    int product = a * b;
+    return product;
+}
+    public static void main(String[] args) {
+        int result = multiply(21,32);
+        System.out.println(result);
+
+
+    }
+}
