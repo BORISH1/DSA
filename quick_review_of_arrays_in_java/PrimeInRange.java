1b87cc5492135f01ae1e7e7aab797e081eb5f7d5
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-06-24 11:27:30 +0530

    Printing a range of prime number

---
+++ quick_review_of_arrays_in_java/PrimeInRange.java
@@ -0,0 +1,24 @@
+package quick_review_of_arrays_in_java;
+
+public class PrimeInRange {
+
+    public static boolean primeRange(int n){
+        for(int i = 2 ; i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+            break;
+        }
+        return true;
+    }
+public static void printNumber(int n){
+    for(int i = 2; i<=n ;i++){
+        if(primeRange(i)){
+            System.out.println(i+" ");
+        }
+    }
+}
+    public static void main(String[] args) {
+        printNumber(10);
+    }
+}
